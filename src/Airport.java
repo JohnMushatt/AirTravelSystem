1eeb110fc5b1949512457e7a5d4b306a2da8b2ea
John Mushatt <john.mushatt@gmail.com>
2018-08-18 20:39:55 -0400

    Can now get distance 'as the crow flys' for routes

--- src/Airport.java
+++ src/Airport.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
 
 public class Airport {
 
+	private String airport_name;
+	private ArrayList<Route> airport_routes = new ArrayList<>();
+	private final double LONGITUDE;
+	private final double LATITUDE;
+	
+	public Airport (String name, double longitude, double latitude) {
+		
+		this.airport_name = name;
+		this.LONGITUDE= longitude;
+		this.LATITUDE = latitude;
+	}
+	
+	public void addRoute(Route route) {
+		
+		this.airport_routes.add(route);
+	}
+	
+	private double getLongitude() {
+		return this.LONGITUDE;
+	}
+	
+	private double getLatitude() {
+		return this.LATITUDE;
+	}
+	private double setRouteDistance(Route route) {
+		
+		double latDistance = Math.toRadians(route.getSourceAirport().getLatitude()
+				- route.getDestinationAirport().getLatitude());
+		double lngDistance = Math.toRadians(route.getSourceAirport().getLongitude()
+				- route.getDestinationAirport().getLongitude());
+
+		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+				+ Math.cos(Math.toRadians(route.getSourceAirport().getLatitude())) * Math.cos(Math.toRadians(route.getDestinationAirport().getLatitude()))
+			    * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+		return Math.round(6371 * c);
+	}
+
+	
 }
--- src/Route.java
+++ src/Route.java
@@ -1,4 +1,45 @@
 
 public class Route {
-
+	
+	private double distance;
+	private double flight_time;
+	private String route_name;
+	private int open_seats;
+	private int total_seats;
+	private Airport source_airport;
+	private Airport destination_airport;
+	
+	public Route (Airport source, Airport destination, String name, int seats, double distance, double flight_time) {
+		this.source_airport= source;
+		this.destination_airport= destination;
+		this.route_name = name;
+		this.total_seats = seats;
+		this.distance = distance;
+		this.flight_time = flight_time;
+		this.open_seats= this.total_seats;
+	}
+	
+	private boolean enoughSeats()
+	{
+		if(open_seats<=total_seats) {
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public boolean addPassenger() {
+		if(enoughSeats()) { 
+			open_seats--;
+			return true;
+		}
+		else
+			return false;
+	}
+	public Airport getSourceAirport() {
+		return this.source_airport;
+	}
+	public Airport getDestinationAirport() {
+		return this.destination_airport;
+	}
 }
